61a2593e99b0c5c69c29c4cd4765b801aeac7e93
mia <miaxiii@gmail.com>
2015-10-30 12:07:44 +0800

    <working> changed database, added all attributes

--- src/Parser.java
+++ src/Parser.java
@@ -15,175 +15,197 @@ import java.util.HashMap;
 import java.util.Iterator;
 
 public class Parser {
-	
-	public Parser(){
-		
+
+	public Parser() {
+
 	}
 
-	public HashMap parseTemp(String fileString){
-		
-		
+	public HashMap parseTemp(String fileString) {
+
 		HashMap<String, ArrayList<String>> tempMap = new HashMap<String, ArrayList<String>>();
-		//tempMap.put("time", )
-		
-		
+		// tempMap.put("time", )
+
 		try {
-	         File inputFile = new File(fileString);
-	         DocumentBuilderFactory dbFactory = 
-	            DocumentBuilderFactory.newInstance();
-	         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-	         Document doc = dBuilder.parse(inputFile);
-	         doc.getDocumentElement().normalize();
-	        // System.out.print("Root element: ");
-	        // System.out.println(doc.getDocumentElement().getNodeName());
-	         NodeList nList = doc.getElementsByTagName("location");
-	         System.out.println("----------------------------");
-	        
-	         
-	         for (int temp = 0; temp < nList.getLength(); temp++) {
-	           System.out.println(temp+"node: ");
-	           Node nlocation = nList.item(temp);
-	        
-	           // get stationid
-	           NodeList stationids =((Element)nlocation).getElementsByTagName("stationId");
-	           Element stationElement = (Element) stationids.item(0);  
-	           String stationid=stationElement.getTextContent();
-	           
-	           // get obstime
-	           NodeList times=((Element)nlocation).getElementsByTagName("time");
-	           Element timeElement = (Element) times.item(0);  
-	           String time=timeElement.getElementsByTagName("obsTime").item(0).getTextContent();
-	           
-	           // get city id , to check if it is Taichung
-	           NodeList parameters=((Element)nlocation).getElementsByTagName("parameter");
-	           Element cityElement = (Element) parameters.item(1);  
-	           String cityParameterName=cityElement.getElementsByTagName("parameterName").item(0).getTextContent();
-	           String cityParameterValue=cityElement.getElementsByTagName("parameterValue").item(0).getTextContent();
-	      
-	            
-	           // if is Taizhong SN=02,
-	           if (cityParameterValue.equals("02")){
-	        	   Element townElement = (Element) parameters.item(3);
-	        	   String townParameterName=townElement.getElementsByTagName("parameterName").item(0).getTextContent();
-		           String townParameterValue=townElement.getElementsByTagName("parameterValue").item(0).getTextContent();
-		       
-		           
-		           NodeList weathers=((Element)nlocation).getElementsByTagName("weatherElement");
-		          
-		           for (int j = 0; j < nList.getLength(); j++) {
-		        	//   System.out.println(((Element)weathers.item(j)).getElementsByTagName("elementName").item(0).getTextContent());
-		        	   if(((Element)weathers.item(j)).getElementsByTagName("elementName").item(0).getTextContent().equals("TEMP")){
-		        		   Node value =((Element)weathers.item(j)).getElementsByTagName("elementValue").item(0);
-		        		   Double tempc=Double.parseDouble(((Element)value).getElementsByTagName("value").item(0).getTextContent());
-		        	       ArrayList<String> values = new ArrayList<String> ();
-		        	       values.add(time);
-		        	       values.add(tempc.toString());
-		        		   tempMap.put(stationid, values);
-		        		   break;
-		        	   }
-		        	 
-		           }
-		           
-	           }
-	         }
-	      } catch (Exception e) {
-	         e.printStackTrace();
-	      }
-		
+			File inputFile = new File(fileString);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(inputFile);
+			doc.getDocumentElement().normalize();
+			// System.out.print("Root element: ");
+			// System.out.println(doc.getDocumentElement().getNodeName());
+			NodeList nList = doc.getElementsByTagName("location");
+			System.out.println("----------------------------");
+
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				System.out.println(temp + "node: ");
+				Node nlocation = nList.item(temp);
+
+				// get stationid
+				NodeList stationids = ((Element) nlocation)
+						.getElementsByTagName("stationId");
+				Element stationElement = (Element) stationids.item(0);
+				String stationid = stationElement.getTextContent();
+
+				// get obstime
+				NodeList times = ((Element) nlocation)
+						.getElementsByTagName("time");
+				Element timeElement = (Element) times.item(0);
+				String time = timeElement.getElementsByTagName("obsTime")
+						.item(0).getTextContent();
+
+				// get city id , to check if it is Taichung
+				NodeList parameters = ((Element) nlocation)
+						.getElementsByTagName("parameter");
+				Element cityElement = (Element) parameters.item(1);
+				String cityParameterName = cityElement
+						.getElementsByTagName("parameterName").item(0)
+						.getTextContent();
+				String cityParameterValue = cityElement
+						.getElementsByTagName("parameterValue").item(0)
+						.getTextContent();
+
+				// if is Taizhong SN=02,
+				if (cityParameterValue.equals("02")) {
+					Element townElement = (Element) parameters.item(3);
+					String townParameterName = townElement
+							.getElementsByTagName("parameterName").item(0)
+							.getTextContent();
+					String townParameterValue = townElement
+							.getElementsByTagName("parameterValue").item(0)
+							.getTextContent();
+
+					NodeList weathers = ((Element) nlocation)
+							.getElementsByTagName("weatherElement");
+                    
+					// add all the weather element to arraylist
+					ArrayList<String> values = new ArrayList<String>();
+					values.add(time);
+					for (int j = 0; j < weathers.getLength(); j++) {
+						
+                       
+						Node value = ((Element) weathers.item(j))
+								.getElementsByTagName("elementValue").item(0);
+						
+						String valuestr = ((Element) value)
+								.getElementsByTagName("value").item(0)
+								.getTextContent();
+						values.add(valuestr);	
+						
+
+					}
+					tempMap.put(stationid, values);
+					
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return tempMap;
+
+	}
+
+	/*public HashMap parseRain(String fileString) {
+
+		HashMap<String, ArrayList<String>> tempMap = new HashMap<String, ArrayList<String>>();
+		// tempMap.put("time", )
+
+		try {
+			File inputFile = new File(fileString);
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+					.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(inputFile);
+			doc.getDocumentElement().normalize();
+			// System.out.print("Root element: ");
+			// System.out.println(doc.getDocumentElement().getNodeName());
+			NodeList nList = doc.getElementsByTagName("location");
+			System.out.println("----------------------------");
+
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				System.out.println(temp + "node: ");
+				Node nlocation = nList.item(temp);
+
+				// get stationid
+				NodeList stationids = ((Element) nlocation)
+						.getElementsByTagName("stationId");
+				Element stationElement = (Element) stationids.item(0);
+				String stationid = stationElement.getTextContent();
+
+				// get obstime
+				NodeList times = ((Element) nlocation)
+						.getElementsByTagName("time");
+				Element timeElement = (Element) times.item(0);
+				String time = timeElement.getElementsByTagName("obsTime")
+						.item(0).getTextContent();
+
+				// get city id , to check if it is Taichung
+				NodeList parameters = ((Element) nlocation)
+						.getElementsByTagName("parameter");
+				Element cityElement = (Element) parameters.item(1);
+				String cityParameterName = cityElement
+						.getElementsByTagName("parameterName").item(0)
+						.getTextContent();
+				String cityParameterValue = cityElement
+						.getElementsByTagName("parameterValue").item(0)
+						.getTextContent();
+
+				// if is Taizhong SN=02,
+				if (cityParameterValue.equals("02")) {
+					Element townElement = (Element) parameters.item(3);
+					String townParameterName = townElement
+							.getElementsByTagName("parameterName").item(0)
+							.getTextContent();
+					String townParameterValue = townElement
+							.getElementsByTagName("parameterValue").item(0)
+							.getTextContent();
+
+					NodeList weathers = ((Element) nlocation)
+							.getElementsByTagName("weatherElement");
+
+					for (int j = 0; j < nList.getLength(); j++) {
+						// System.out.println(((Element)weathers.item(j)).getElementsByTagName("elementName").item(0).getTextContent());
+						if (((Element) weathers.item(j))
+								.getElementsByTagName("elementName").item(0)
+								.getTextContent().equals("NOW")) {
+							Node value = ((Element) weathers.item(j))
+									.getElementsByTagName("elementValue").item(
+											0);
+							Double tempc = Double.parseDouble(((Element) value)
+									.getElementsByTagName("value").item(0)
+									.getTextContent());
+							ArrayList<String> values = new ArrayList<String>();
+							values.add(time);
+							values.add(tempc.toString());
+							tempMap.put(stationid, values);
+							break;
+						}
+
+					}
+
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		Iterator it = tempMap.entrySet().iterator();
-	    while (it.hasNext()){
-	      HashMap.Entry pair = (HashMap.Entry)it.next();
-	      String id = (String) pair.getKey();
-	      String time = (String)((ArrayList)pair.getValue()).get(0);  //get time stamp string
-	      String value = (String)((ArrayList)pair.getValue()).get(1);  //get value string
-	      System.out.println(pair.getKey() + " : " + time + value);      
-	      
-	    }
-	    return tempMap;
+		while (it.hasNext()) {
+			HashMap.Entry pair = (HashMap.Entry) it.next();
+			String id = (String) pair.getKey();
+			String time = (String) ((ArrayList) pair.getValue()).get(0); // get
+																			// time
+																			// stamp
+																			// string
+			String value = (String) ((ArrayList) pair.getValue()).get(1); // get
+																			// value
+																			// string
+			System.out.println(pair.getKey() + " : " + time + value);
 
-	}
-	
-public HashMap parseRain(String fileString){
-		
-	HashMap<String, ArrayList<String>> tempMap = new HashMap<String, ArrayList<String>>();
-	//tempMap.put("time", )
-	
-	
-	try {
-         File inputFile = new File(fileString);
-         DocumentBuilderFactory dbFactory = 
-            DocumentBuilderFactory.newInstance();
-         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-         Document doc = dBuilder.parse(inputFile);
-         doc.getDocumentElement().normalize();
-        // System.out.print("Root element: ");
-        // System.out.println(doc.getDocumentElement().getNodeName());
-         NodeList nList = doc.getElementsByTagName("location");
-         System.out.println("----------------------------");
-        
-         
-         for (int temp = 0; temp < nList.getLength(); temp++) {
-           System.out.println(temp+"node: ");
-           Node nlocation = nList.item(temp);
-        
-           // get stationid
-           NodeList stationids =((Element)nlocation).getElementsByTagName("stationId");
-           Element stationElement = (Element) stationids.item(0);  
-           String stationid=stationElement.getTextContent();
-           
-           // get obstime
-           NodeList times=((Element)nlocation).getElementsByTagName("time");
-           Element timeElement = (Element) times.item(0);  
-           String time=timeElement.getElementsByTagName("obsTime").item(0).getTextContent();
-           
-           // get city id , to check if it is Taichung
-           NodeList parameters=((Element)nlocation).getElementsByTagName("parameter");
-           Element cityElement = (Element) parameters.item(1);  
-           String cityParameterName=cityElement.getElementsByTagName("parameterName").item(0).getTextContent();
-           String cityParameterValue=cityElement.getElementsByTagName("parameterValue").item(0).getTextContent();
-      
-            
-           // if is Taizhong SN=02,
-           if (cityParameterValue.equals("02")){
-        	   Element townElement = (Element) parameters.item(3);
-        	   String townParameterName=townElement.getElementsByTagName("parameterName").item(0).getTextContent();
-	           String townParameterValue=townElement.getElementsByTagName("parameterValue").item(0).getTextContent();
-	       
-	           
-	           NodeList weathers=((Element)nlocation).getElementsByTagName("weatherElement");
-	          
-	           for (int j = 0; j < nList.getLength(); j++) {
-	        	//   System.out.println(((Element)weathers.item(j)).getElementsByTagName("elementName").item(0).getTextContent());
-	        	   if(((Element)weathers.item(j)).getElementsByTagName("elementName").item(0).getTextContent().equals("NOW")){
-	        		   Node value =((Element)weathers.item(j)).getElementsByTagName("elementValue").item(0);
-	        		   Double tempc=Double.parseDouble(((Element)value).getElementsByTagName("value").item(0).getTextContent());
-	        	       ArrayList<String> values = new ArrayList<String> ();
-	        	       values.add(time);
-	        	       values.add(tempc.toString());
-	        		   tempMap.put(stationid, values);
-	        		   break;
-	        	   }
-	        	 
-	           }
-	           
-           }
-         }
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
-	
-	Iterator it = tempMap.entrySet().iterator();
-    while (it.hasNext()){
-      HashMap.Entry pair = (HashMap.Entry)it.next();
-      String id = (String) pair.getKey();
-      String time = (String)((ArrayList)pair.getValue()).get(0);  //get time stamp string
-      String value = (String)((ArrayList)pair.getValue()).get(1);  //get value string
-      System.out.println(pair.getKey() + " : " + time + value);      
-      
-    }
-    return tempMap;
+		}
+		return tempMap;
 
-
-	}
+	}*/
 
 }
--- src/TaiwanWeather.java
+++ src/TaiwanWeather.java
@@ -20,43 +20,40 @@ import java.util.Iterator;
 import java.sql.Timestamp;
 import java.util.Date;
 
-
-
 public class TaiwanWeather {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-       
-        
-      //  saveURLToFile("http://opendata.cwb.gov.tw/member/opendataapi?dataid=O-A0001-001&authorizationkey=CWB-9488A5D7-3B34-449B-81E9-51836A5A6CA6","src/main/resources/A0001.xml");
-      //  saveURLToFile("http://opendata.cwb.gov.tw/member/opendataapi?dataid=O-A0002-001&authorizationkey=CWB-9488A5D7-3B34-449B-81E9-51836A5A6CA6","src/main/resources/A0002.xml");
-        Parser parser=new Parser();
-        HashMap tempMap=parser.parseTemp("src/main/resources/A0001.xml");  
-        HashMap rainMap=parser.parseRain("src/main/resources/A0002.xml"); 
-        writehashMapToDB(true,tempMap);
-        writehashMapToDB(false,rainMap);
+		saveURLToFile("http://opendata.cwb.gov.tw/member/opendataapi?dataid=O-A0001-001&authorizationkey=CWB-9488A5D7-3B34-449B-81E9-51836A5A6CA6","src/main/resources/A0001.xml");
+		saveURLToFile("http://opendata.cwb.gov.tw/member/opendataapi?dataid=O-A0002-001&authorizationkey=CWB-9488A5D7-3B34-449B-81E9-51836A5A6CA6","src/main/resources/A0002.xml");
+		Parser parser = new Parser();
+		HashMap tempMap = parser.parseTemp("src/main/resources/A0001.xml");
+		HashMap rainMap = parser.parseTemp("src/main/resources/A0002.xml");
+		writehashMapToDB(true, tempMap);
+		writehashMapToDB(false, rainMap);
 
-    }
-    
-    public static void saveURLToFile(String URLString,String fileName){
-    	URL URL=null;
+	}
+
+	public static void saveURLToFile(String URLString, String fileName) {
+		URL URL = null;
 		try {
 			URL = new URL(URLString);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	
-    	try {
+
+		try {
 			FileUtils.copyURLToFile(URL, new File(fileName));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	System.out.println("file save successfully!" + fileName);
-    	
-    }
-    public static void writehashMapToDB(Boolean isTemp, HashMap hashMap){
+		System.out.println("file save successfully!" + fileName);
+
+	}
+
+	public static void writehashMapToDB(Boolean isTemp, HashMap hashMap){
     	
     	
         Connection con = null;
@@ -66,7 +63,7 @@ public class TaiwanWeather {
 
 
     
-        String url = "jdbc:postgresql://132.147.88.190:5432/ChillerProject";
+        String url = "jdbc:postgresql://132.147.88.190:5433/ems3";
         String user = "ecoadm";
         String password = "ev093qer";
         
@@ -86,21 +83,53 @@ public class TaiwanWeather {
             
          //   Date date= new Date();
   	     //   Timestamp timestamp=new Timestamp(date.getTime());
-            
+            if (isTemp){
             Iterator it = hashMap.entrySet().iterator();
     	    while (it.hasNext()){
     	      HashMap.Entry pair = (HashMap.Entry)it.next();
     	      String id = (String) pair.getKey();
     	      String time = (String)((ArrayList)pair.getValue()).get(0);  //get time stamp string
-    	      String value = (String)((ArrayList)pair.getValue()).get(1);  //get value string
-    	      System.out.println(pair.getKey() + " : " + time + value); 
-    	      if (isTemp){
-    	      query= "insert into \"Taiwan_Temp\" values(\'"+time+"\',\'"+id+"\',\'"+value+"\')";}
-    	      else {
-    	    	  query= "insert into \"Taiwan_Rain\" values(\'"+time+"\',\'"+id+"\',\'"+value+"\')";
-    	      }
-    	    	  
-              st.executeUpdate(query);
+    	      String elev = (String)((ArrayList)pair.getValue()).get(1);
+    	      String wdir = (String)((ArrayList)pair.getValue()).get(2);
+    	      String wdsd = (String)((ArrayList)pair.getValue()).get(3);
+    	      String temp = (String)((ArrayList)pair.getValue()).get(4);
+    	      String humd = (String)((ArrayList)pair.getValue()).get(5);
+    	      String pres = (String)((ArrayList)pair.getValue()).get(6);
+    	      String sun = (String)((ArrayList)pair.getValue()).get(7);
+    	      String h24r = (String)((ArrayList)pair.getValue()).get(8);
+    	      String ws15m = (String)((ArrayList)pair.getValue()).get(9);
+    	      String wd15m = (String)((ArrayList)pair.getValue()).get(10);
+    	      String ws15t = (String)((ArrayList)pair.getValue()).get(11);
+    	    
+    	      //System.out.println(pair.getKey() + " : " + time + value); 
+    	      
+    	      query= "insert into \"taiwan_temp\" values(\'"+time+"\',\'"+id+"\',\'"+elev+"\',\'"+wdir+"\',\'"+wdsd+"\',\'"+temp+"\',\'"+humd+"\',\'"+pres+"\',\'"+sun+"\',\'"+h24r+"\',\'"+ws15m+"\',\'"+wd15m+"\',\'"+ws15t+"\')";
+    	      st.executeUpdate(query);  
+    	    }
+    	      
+              
+    	    }else 
+    	    {
+    	    	Iterator it = hashMap.entrySet().iterator();
+        	    while (it.hasNext()){
+        	      HashMap.Entry pair = (HashMap.Entry)it.next();
+        	      String id = (String) pair.getKey();
+        	      String time = (String)((ArrayList)pair.getValue()).get(0);  //get time stamp string
+        	      String elev = (String)((ArrayList)pair.getValue()).get(1);
+        	      String rain = (String)((ArrayList)pair.getValue()).get(2);
+        	      String min10 = (String)((ArrayList)pair.getValue()).get(3);
+        	      String hour3 = (String)((ArrayList)pair.getValue()).get(4);
+        	      String hour6 = (String)((ArrayList)pair.getValue()).get(5);
+        	      String hour12 = (String)((ArrayList)pair.getValue()).get(6);
+        	      String hour24 = (String)((ArrayList)pair.getValue()).get(7);
+        	      String now = (String)((ArrayList)pair.getValue()).get(8);
+        	      
+        	    
+        	      //System.out.println(pair.getKey() + " : " + time + value); 
+        	      
+        	      query= "insert into \"taiwan_rain\" values(\'"+time+"\',\'"+id+"\',\'"+elev+"\',\'"+rain+"\',\'"+min10+"\',\'"+hour3+"\',\'"+hour6+"\',\'"+hour12+"\',\'"+hour24+"\',\'"+now+"\')";
+        	      st.executeUpdate(query);
+        	    }
     	    }
            
            
@@ -129,6 +158,5 @@ public class TaiwanWeather {
             }
         }
     }
-    
-   
+
 }
